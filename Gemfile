7213cca212c264b6e9ec2612bc5afa624f0bfece
ksigei <sigeikiprono4@gmail.com>
2022-10-21 23:33:29 +0300

    Add bootstrap

--- Gemfile
+++ Gemfile
@@ -72,3 +72,6 @@ group :test do
 end
 
 gem 'devise'  
+gem "cssbundling-rails", "~> 1.1"
+
+gem "jsbundling-rails", "~> 1.0"
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,6 @@
 Rails.application.routes.draw do
-  get 'places/index'
-  get 'places/show'
-  get 'places/create'
-  get 'places/new'
-  get 'places/edit'
-  get 'places/destroy'
-  get 'users/index'
-  # devise_for :users
+  resources :users
+  devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
